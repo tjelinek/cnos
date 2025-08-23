d4cadb973d271b676c10cff48ea43c526f10a3c1
tjelinek <tomasjelinek96@gmail.com>
2025-08-23 19:24:22 +0200

    running detections directly from cnos model

--- infer_masks.py
+++ infer_masks.py
@@ -1,35 +1,31 @@
+import sys
 from pathlib import Path
+
+from repositories.cnos.src.model.fast_sam import FastSAM
+
+sys.path.append(str((Path(__file__).parent).resolve()))
 import numpy as np
 from PIL import Image
 import cv2
 from hydra import compose, initialize_config_dir
+from hydra.utils import instantiate
 from omegaconf import DictConfig
 from tqdm import tqdm
 
-from repositories.cnos.src.model.sam import CustomSamAutomaticMaskGenerator, load_sam
 from utils.image_utils import overlay_mask
-
-
-def build_generator(cfg: DictConfig):
-    seg_cfg = cfg.model.segmentor_model
-    sam = load_sam(
-        model_type=seg_cfg.sam.model_type,
-        checkpoint_dir=seg_cfg.sam.checkpoint_dir
-    ).to('cuda')
-    return CustomSamAutomaticMaskGenerator(
-        sam=sam,
-        points_per_batch=seg_cfg.points_per_batch,
-        min_mask_region_area=seg_cfg.min_mask_region_area,
-        stability_score_thresh=seg_cfg.stability_score_thresh,
-        box_nms_thresh=seg_cfg.box_nms_thresh,
-        segmentor_width_size=seg_cfg.segmentor_width_size,
-    )
+from repositories.cnos.src.model.detector import CNOS
 
 
 def infer_masks_for_folder(folder: Path, cfg: DictConfig):
-    gen = build_generator(cfg)
+    cnos_model: CNOS = instantiate(cfg.model).to('cuda')
+    cnos_model.move_to_device()
+    if hasattr(cnos_model.segmentor_model, "predictor"):
+        cnos_model.segmentor_model.predictor.model = (
+            cnos_model.segmentor_model.predictor.model.to('cuda')
+        )
+    else:
+        cnos_model.segmentor_model.model.setup_model(device=torch.device('cuda'), verbose=True)
     folder = folder.resolve()
-    base_dir = folder.parent
 
     for sequence in tqdm(sorted(folder.iterdir()), desc=f"[{folder}] Sequences"):
         image_folder = sequence / 'rgb'
@@ -38,16 +34,19 @@ def infer_masks_for_folder(folder: Path, cfg: DictConfig):
         if not image_folder.exists():
             continue
 
-        proposals_dir = sequence / "cnos_sam_proposals"
-        visual_dir = sequence / "cnos_sam_visual"
+        segment_model_name = 'fastsam' if isinstance(cnos_model.segmentor_model, FastSAM) else 'sam'
+        proposals_dir = sequence / f"cnos_{segment_model_name}_proposals"
+        visual_dir = sequence / f"cnos_{segment_model_name}_visual"
         proposals_dir.mkdir(parents=True, exist_ok=True)
         visual_dir.mkdir(parents=True, exist_ok=True)
 
         for img_path in tqdm(sorted(image_folder.iterdir()), leave=False, desc=f"Images in {sequence.name}"):
             img_name = img_path.stem
             img = np.array(Image.open(img_path).convert("RGB"))
-            masks = gen.generate(img)
+            detections = cnos_model.get_filtered_detections(img)
 
+            masks = detections.masks
+            detections_descriptors = cnos_model.descriptor_model(img, detections)
             for i, m in enumerate(masks):
                 mask_uint8 = (m["segmentation"].astype(np.uint8) * 255)
                 vis = overlay_mask(img, m["segmentation"].astype(np.float32))
@@ -56,9 +55,17 @@ def infer_masks_for_folder(folder: Path, cfg: DictConfig):
 
 
 if __name__ == "__main__":
+    model = 'cnos_fast'  # either 'cnos' or 'cnos_fast'
+
+    sys.path.append('/repositories/cnos')
     cfg_dir = (Path(__file__).parent / "configs").resolve()
     with initialize_config_dir(config_dir=str(cfg_dir), version_base=None):
-        cfg = compose(config_name="run_inference")
+        cfg = compose(
+            config_name="run_inference",
+            overrides=[
+                f"model={model}",
+            ],
+        )
 
     base_path = Path('/mnt/personal/jelint19/data/')
     bop_path = base_path / 'bop'

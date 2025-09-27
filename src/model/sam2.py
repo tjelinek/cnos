cc1a211a65857e9237da6d682a95c22115d1ce39
tjelinek <tomasjelinek96@gmail.com>
2025-09-27 22:47:23 +0200

    sam2 now has its own file

--- src/model/sam2.py
+++ src/model/sam2.py
@@ -1,36 +1,12 @@
-from segment_anything import (
-    sam_model_registry,
-    SamPredictor,
-    SamAutomaticMaskGenerator,
-)
-from segment_anything.modeling import Sam
-from segment_anything.utils.amg import MaskData, generate_crop_boxes, rle_to_mask
-
+# SAM2 imports
+from sam2.build_sam import build_sam2
+from sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator
 
 import logging
 import numpy as np
 import torch
-from torchvision.ops.boxes import batched_nms, box_area  # type: ignore
-import os.path as osp
 from pathlib import Path
 from typing import Any, Dict, List, Optional, Union
-import cv2
-import torch.nn.functional as F
-
-pretrained_weight_dict = {
-    "vit_l": "sam_vit_l_0b3195.pth",  # 1250MB
-    "vit_b": "sam_vit_b_01ec64.pth",  # 375MB
-    "vit_h": "sam_vit_h_4b8939.pth",  # 2500MB
-}
-
-
-def load_sam(model_type, checkpoint_dir):
-    logging.info(f"Loading SAM model from {checkpoint_dir}")
-    sam = sam_model_registry[model_type](
-        checkpoint=osp.join(checkpoint_dir, pretrained_weight_dict[model_type])
-    )
-    sam._is_sam2 = False  # Mark as SAM1
-    return sam
 
 
 def load_sam2(
@@ -41,12 +17,10 @@ def load_sam2(
         device: str = "cuda",
         **kwargs
 ) -> torch.nn.Module:
-
+    """Load SAM 2 model."""
     from hydra.core.global_hydra import GlobalHydra
     import sam2
-    from sam2.build_sam import build_sam2
 
-    """Load SAM 2 model."""
     checkpoint_dir = Path(checkpoint_dir)
 
     # Auto-resolve checkpoint and config files
@@ -70,19 +44,18 @@ def load_sam2(
         GlobalHydra.instance().clear()
     from hydra import initialize_config_dir
     with initialize_config_dir(config_dir=str(cfg_dir.resolve()), version_base=None, job_name="sam2"):
-        sam2 = build_sam2(config_file, str(checkpoint_path), device=device)
-    sam2._is_sam2 = True  # Mark as SAM2
+        sam2_model = build_sam2(config_file, str(checkpoint_path), device=device)
 
-    return sam2
+    return sam2_model
 
 
 def _resolve_sam2_files(model_type: str) -> tuple[str, str]:
     """Resolve SAM2 checkpoint and config file names from model type."""
     sam2_files = {
-        'sam2.1_hiera_tiny': ('sam2.1_hiera_tiny.pt', 'sam2.1_hiera_t.yaml'),
-        'sam2.1_hiera_small': ('sam2.1_hiera_small.pt', 'sam2.1_hiera_s.yaml'),
-        'sam2.1_hiera_base_plus': ('sam2.1_hiera_base_plus.pt', 'sam2.1_hiera_b+.yaml'),
-        'sam2.1_hiera_large': ('sam2.1_hiera_large.pt', 'sam2.1_hiera_l.yaml'),
+        'sam2.1_hiera_tiny': ('sam2.1_hiera_tiny.pt', 'sam2.1/sam2.1_hiera_t.yaml'),
+        'sam2.1_hiera_small': ('sam2.1_hiera_small.pt', 'sam2.1/sam2.1_hiera_s.yaml'),
+        'sam2.1_hiera_base_plus': ('sam2.1_hiera_base_plus.pt', 'sam2.1/sam2.1_hiera_b+.yaml'),
+        'sam2.1_hiera_large': ('sam2.1_hiera_large.pt', 'sam2.1/sam2.1_hiera_l.yaml'),
         'sam2_hiera_tiny': ('sam2_hiera_tiny.pt', 'sam2_hiera_t.yaml'),
         'sam2_hiera_small': ('sam2_hiera_small.pt', 'sam2_hiera_s.yaml'),
         'sam2_hiera_base_plus': ('sam2_hiera_base_plus.pt', 'sam2_hiera_b+.yaml'),
@@ -94,42 +67,22 @@ def _resolve_sam2_files(model_type: str) -> tuple[str, str]:
     else:
         checkpoint_file = f"{model_type}.pt"
         if 'tiny' in model_type:
-            config_file = f"{model_type.split('_')[0]}_hiera_t.yaml"
+            config_file = f"{model_type.split('_')[0]}/sam2.1_hiera_t.yaml"
         elif 'small' in model_type:
-            config_file = f"{model_type.split('_')[0]}_hiera_s.yaml"
+            config_file = f"{model_type.split('_')[0]}/sam2.1_hiera_s.yaml"
         elif 'base' in model_type:
-            config_file = f"{model_type.split('_')[0]}_hiera_b+.yaml"
+            config_file = f"{model_type.split('_')[0]}/sam2.1_hiera_b+.yaml"
         elif 'large' in model_type:
-            config_file = f"{model_type.split('_')[0]}_hiera_l.yaml"
+            config_file = f"{model_type.split('_')[0]}/sam2.1_hiera_l.yaml"
         else:
             config_file = f"{model_type}.yaml"
         return checkpoint_file, config_file
 
 
-def load_sam_predictor(model_type, checkpoint_dir, device):
-    logging.info(f"Loading SAM model from {checkpoint_dir}")
-    sam = sam_model_registry[model_type](
-        checkpoint=osp.join(checkpoint_dir, pretrained_weight_dict[model_type])
-    )
-    sam.to(device=device)
-    predictor = SamPredictor(sam)
-    return predictor
-
-
-def load_sam_mask_generator(model_type, checkpoint_dir, device):
-    logging.info(f"Loading SAM model from {checkpoint_dir}")
-    sam = sam_model_registry[model_type](
-        checkpoint=osp.join(checkpoint_dir, pretrained_weight_dict[model_type])
-    )
-    sam.to(device=device)
-    mask_generator = SamAutomaticMaskGenerator(sam, output_mode="coco_rle")
-    return mask_generator
-
-
-class CustomSamAutomaticMaskGenerator(SamAutomaticMaskGenerator):
+class CustomSamAutomaticMaskGenerator:
     def __init__(
             self,
-            sam: Sam,
+            sam,
             min_mask_region_area: int = 0,
             points_per_batch: int = 64,
             stability_score_thresh: float = 0.95,
@@ -138,36 +91,22 @@ class CustomSamAutomaticMaskGenerator(SamAutomaticMaskGenerator):
             segmentor_width_size=None,
     ):
         self.segmentor_width_size = segmentor_width_size
-        self.is_sam2 = hasattr(sam, '_is_sam2') and sam._is_sam2
+        self.sam = sam
 
-        if self.is_sam2:
-            # SAM2 parameters mapping
-            sam2_params = {
-                'model': sam,
-                'points_per_batch': points_per_batch,
-                'pred_iou_thresh': stability_score_thresh,
-                'stability_score_thresh': stability_score_thresh,
-                'box_nms_thresh': box_nms_thresh,
-                'min_mask_region_area': min_mask_region_area,
-            }
-            if crop_overlap_ratio != 512 / 1500:  # only add if different from default
-                sam2_params['crop_overlap_ratio'] = crop_overlap_ratio
+        # SAM2 parameters mapping
+        sam2_params = {
+            'model': sam,
+            'points_per_batch': points_per_batch,
+            'pred_iou_thresh': stability_score_thresh,
+            'stability_score_thresh': stability_score_thresh,
+            'box_nms_thresh': box_nms_thresh,
+            'min_mask_region_area': min_mask_region_area,
+        }
+        if crop_overlap_ratio != 512 / 1500:  # only add if different from default
+            sam2_params['crop_overlap_ratio'] = crop_overlap_ratio
 
-            self.sam = sam
-            from sam2.automatic_mask_generator import SAM2AutomaticMaskGenerator
-            self._sam2_generator = SAM2AutomaticMaskGenerator(**sam2_params)
-            logging.info(f"Init CustomSamAutomaticMaskGenerator with SAM2 done!")
-        else:
-            SamAutomaticMaskGenerator.__init__(
-                self,
-                sam,
-                min_mask_region_area=min_mask_region_area,
-                points_per_batch=points_per_batch,
-                stability_score_thresh=stability_score_thresh,
-                box_nms_thresh=box_nms_thresh,
-                crop_overlap_ratio=crop_overlap_ratio,
-            )
-            logging.info(f"Init CustomSamAutomaticMaskGenerator with SAM1 done!")
+        self._sam2_generator = SAM2AutomaticMaskGenerator(**sam2_params)
+        logging.info(f"Init CustomSamAutomaticMaskGenerator with SAM2 done!")
 
     @property
     def model(self):
